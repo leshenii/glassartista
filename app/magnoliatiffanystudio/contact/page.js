83f56bbd28dad86743e095580a835d01b4dc0b7a
lesIII <gulyas_laszlo@outlook.com>
2025-09-07 03:37:25 +0200

    under development disclaimer

--- app/magnoliatiffanystudio/contact/page.js
+++ app/magnoliatiffanystudio/contact/page.js
@@ -8,7 +8,7 @@ import {TbExternalLink} from "react-icons/tb";
 
 export default function ContactPage() {
     return (
-        <div className="flex flex-row gap-3 w-screen justify-end items-center h-screen-minus-navbar-desktop">
+        <div className="flex flex-row gap-3 w-screen items-center h-screen-minus-navbar-desktop pb-16">
             <div className="flex flex-col gap-4 w-full justify-center items-start mx-60">
                 <h2 className="text-5xl allura-regular underline decoration-2 underline-offset-8">Nyíri Eszter</h2>
                 <div className="flex flex-row items-center gap-2 ">
@@ -28,8 +28,8 @@ export default function ContactPage() {
                     Magnólia Tiffanystúdió <TbExternalLink size={15}/>
                 </div>
             </div>
-            <div className="flex flex-col w-1/2">
-                e-mail
+            <div className="flex flex-col w-full text-gray-600 items-start">
+                e-mail küldési űrlap fejlesztés alatt
             </div>
         </div>
     )
